0eb84546a2b1b9b005d7171fd18145dd03abf62a
flamebird797 <amv6@illinois.edu>
2026-08-18 11:13:46 -0500

    Got the physics working

---
+++ integrator.hpp
@@ -0,0 +1,5 @@
+#include "proportional_nav.hpp"
+
+//integration for the bird's position and velocity
+void euler_integration(double dt, Vector& position, Vector& velocity, const Vector& acceleration) {
+};
